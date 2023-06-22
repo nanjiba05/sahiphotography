1d8e6ca2a4a6eace24e5240092b81949d5521e5a
nanjiba05 <121885801+nanjiba05@users.noreply.github.com>
2023-06-23 07:42:05 +0900

    sahira

---
+++ script.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+  alert("Hello Sensei");
+};
+
+window.addEventListener("DOMContentLoaded", function() {
+  var footer = document.querySelector("footer");
+  footer.innerHTML = "Thank you for visiting my website.";
+  footer.innerHTML += "<br>Name: Nanjiba Ibnath Sahira";
+  footer.innerHTML += "<br>Student ID: 23224559";
+});
